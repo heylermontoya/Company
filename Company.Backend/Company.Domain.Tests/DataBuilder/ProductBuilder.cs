7595af8aa130063e4e8075a74e31ca67187fd8ae
HeylerMontoya <heylers03@gmail.com>
2024-12-19 21:31:31 -0500

    Se sube como ejemplo pruebas unitarias a la logica de negocio en el servicio de ProductService

---
+++ Company.Backend/Company.Domain.Tests/DataBuilder/ProductBuilder.cs
@@ -0,0 +1,73 @@
+﻿using Company.Infrastructure;
+
+namespace Company.Domain.Tests.DataBuilder
+{
+    public class ProductBuilder
+    {
+        private int _productId;
+        private string _productName;
+        private int _inventory;
+        private decimal _price;
+        private bool _isDeleted;
+        private ICollection<Transaction> _transactions;
+
+        public ProductBuilder()
+        {
+            _productId = 0; // Default ID
+            _productName = "Default Product Name";
+            _inventory = 0; // Default inventory
+            _price = 0.0m; // Default price
+            _isDeleted = false; // Default to not deleted
+            _transactions = new List<Transaction>(); // Empty transaction list by default
+        }
+
+        public Product Build()
+        {
+            return new Product
+            {
+                Productid = _productId,
+                Productname = _productName,
+                Inventory = _inventory,
+                Price = _price,
+                Isdeleted = _isDeleted,
+                Transactions = _transactions
+            };
+        }
+
+        public ProductBuilder WithProductId(int productId)
+        {
+            _productId = productId;
+            return this;
+        }
+
+        public ProductBuilder WithProductName(string productName)
+        {
+            _productName = productName;
+            return this;
+        }
+
+        public ProductBuilder WithInventory(int inventory)
+        {
+            _inventory = inventory;
+            return this;
+        }
+
+        public ProductBuilder WithPrice(decimal price)
+        {
+            _price = price;
+            return this;
+        }
+
+        public ProductBuilder WithIsDeleted(bool isDeleted)
+        {
+            _isDeleted = isDeleted;
+            return this;
+        }
+
+        public ProductBuilder WithTransactions(ICollection<Transaction> transactions)
+        {
+            _transactions = transactions;
+            return this;
+        }
+    }
+}
---
+++ Company.Backend/Company.Domain.Tests/ProductServiceTest.cs
@@ -0,0 +1,402 @@
+﻿using Company.Domain.Exceptions;
+using Company.Domain.Ports;
+using Company.Domain.Services;
+using Company.Domain.Tests.DataBuilder;
+using Company.Infrastructure;
+using NSubstitute;
+using System.Linq.Expressions;
+
+namespace Company.Domain.Tests
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+        private ProductService Service { get; set; } = default!;
+        private IGenericRepository<Product> ProductRepository { get; set; } = default!;
+        private ProductBuilder ProductBuilder { get; set; } = default!;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            ProductRepository = Substitute.For<IGenericRepository<Product>>();
+
+            Service = new(
+                ProductRepository
+            );
+
+            ProductBuilder = new();
+        }
+
+        [TestMethod]
+        public async Task GetProductsAllAsync_Ok()
+        {
+            //Arrange
+            List<Product> product = [
+                ProductBuilder
+                    .Build()
+            ];
+                
+            ProductRepository.GetAsync(
+                product => !product.Isdeleted
+            ).ReturnsForAnyArgs(product);
+
+            //Act
+            List<Product> response = await Service.GetProductsAllAsync();
+
+            //Assert
+            Assert.IsNotNull(response);
+            Assert.AreEqual(product.Count, response.Count);
+            await ProductRepository
+                .ReceivedWithAnyArgs(1)
+                .GetAsync(
+                    Arg.Any<Expression<Func<Product, bool>>?>()
+                );
+        }
+        
+        [TestMethod]
+        public async Task GetProductByProductIdAsync_Ok()
+        {
+            //Arrange
+            int productId = 1;
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .Build();
+                            
+            ProductRepository.FindByAlternateKeyAsync(
+                Arg.Any<Expression < Func<Product, bool>>>()
+            ).ReturnsForAnyArgs(product);
+
+            //Act
+            Product response = await Service.GetProductByProductIdAsync(productId);
+
+            //Assert
+            Assert.IsNotNull(response);
+            Assert.AreEqual(product.Productid, response.Productid);
+            await ProductRepository
+                .ReceivedWithAnyArgs(1)
+                .FindByAlternateKeyAsync(
+                    Arg.Any<Expression<Func<Product, bool>>>()
+                );
+        }
+        
+        [TestMethod]
+        public async Task GetProductByProductIdAsync_Failed()
+        {
+            //Arrange
+            int productId = 1;
+
+            //Act
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.GetProductByProductIdAsync(productId);
+            });
+
+            //Assert
+            Assert.AreEqual(
+                $"El producto con ID {productId} no existe.", 
+                ex.Message
+            );            
+        }
+
+        [TestMethod]
+        public async Task CreateProductAsync_Ok()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = 10;
+            decimal price = 1000;
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .Build();
+
+            IEnumerable<Product> listproducts = [product];
+                
+            ProductRepository.GetAsync().ReturnsForAnyArgs(listproducts);
+            ProductRepository.AddAsync(product).ReturnsForAnyArgs(product);
+
+            //Act
+            Product response = await Service.CreateProductAsync(productName,inventory,price);
+
+            //Assert
+            Assert.IsNotNull(response);
+            Assert.AreEqual(product.Productid, response.Productid);
+            Assert.AreEqual(product.Productname, response.Productname);
+            Assert.AreEqual(product.Inventory, response.Inventory);
+            Assert.AreEqual(product.Price, response.Price);
+            Assert.IsFalse(response.Isdeleted);
+            await ProductRepository.ReceivedWithAnyArgs(1).GetAsync();
+            await ProductRepository.ReceivedWithAnyArgs(1).AddAsync(Arg.Any<Product>());
+        }
+
+        [TestMethod]
+        public async Task CreateProductAsync_ProductExistedFailed()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = 10;
+            decimal price = 1000;
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductName(productName)
+                .WithProductId(productId)
+                .Build();
+
+            IEnumerable<Product> listproducts = [product];
+
+            ProductRepository.GetAsync().ReturnsForAnyArgs(listproducts);
+
+            //Act
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.CreateProductAsync(productName, inventory, price);
+            });
+
+            //Assert
+            Assert.AreEqual(
+                $"Ya existe un producto con el nombre '{productName}'.",
+                ex.Message
+            );
+            await ProductRepository.ReceivedWithAnyArgs(1).GetAsync();
+        }
+        
+        [TestMethod]
+        public async Task CreateProductAsync_ProductWithPriceFailed()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = 10;
+            decimal price = -1000;
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .Build();
+
+            IEnumerable<Product> listproducts = [product];
+
+            ProductRepository.GetAsync().ReturnsForAnyArgs(listproducts);
+
+            //Act
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.CreateProductAsync(productName, inventory, price);
+            });
+
+            //Assert
+            Assert.AreEqual(
+                "El precio debe ser mayor o igual a cero.",
+                ex.Message
+            );
+            await ProductRepository.ReceivedWithAnyArgs(1).GetAsync();
+        }
+        
+        [TestMethod]
+        public async Task CreateProductAsync_ProductWithInventoryFailed()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = -10;
+            decimal price = 1000;
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .Build();
+
+            IEnumerable<Product> listproducts = [product];
+
+            ProductRepository.GetAsync().ReturnsForAnyArgs(listproducts);
+
+            //Act
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.CreateProductAsync(productName, inventory, price);
+            });
+
+            //Assert
+            Assert.AreEqual(
+                "El inventario debe ser mayor o igual a cero.",
+                ex.Message
+            );
+            await ProductRepository.ReceivedWithAnyArgs(1).GetAsync();
+        }
+
+        [TestMethod]
+        public async Task UpdateProductAsync_Ok()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = 10;
+            decimal price = 1000;
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .Build();
+
+            IEnumerable<Product> listproducts = [product];
+
+            ProductRepository.GetByIdAsync(productId).ReturnsForAnyArgs(product);
+            ProductRepository.GetAsync().ReturnsForAnyArgs(listproducts);
+            ProductRepository.UpdateAsync(product).ReturnsForAnyArgs(product);
+
+            //Act
+            Product response = await Service.UpdateProductAsync(
+                productId,
+                productName, 
+                inventory, 
+                price
+            );
+
+            //Assert
+            Assert.IsNotNull(response);
+            Assert.AreEqual(product.Productid, response.Productid);
+            Assert.AreEqual(product.Productname, response.Productname);
+            Assert.AreEqual(product.Inventory, response.Inventory);
+            Assert.AreEqual(product.Price, response.Price);
+            Assert.IsFalse(response.Isdeleted);
+            await ProductRepository.ReceivedWithAnyArgs(1).GetByIdAsync(Arg.Any<object>());
+            await ProductRepository.ReceivedWithAnyArgs(1).GetAsync();
+            await ProductRepository.ReceivedWithAnyArgs(1).UpdateAsync(Arg.Any<Product>());
+        }
+        
+        [TestMethod]
+        public async Task UpdateProductAsync_ProductNotExistFailed()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = 10;
+            decimal price = 1000;
+            int productId = 1;
+           
+            //Act            
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.UpdateProductAsync(
+                   productId,
+                   productName,
+                   inventory,
+                   price
+                );
+            });
+
+            //Assert
+            Assert.AreEqual(
+                $"El producto con ID {productId} no existe.",
+                ex.Message
+            );            
+        }
+        
+        [TestMethod]
+        public async Task UpdateProductAsync_ProductBeforeDeletedFailed()
+        {
+            //Arrange
+            string productName = "Caja 1";
+            int inventory = 10;
+            decimal price = 1000;
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .WithIsDeleted(true)
+                .Build();
+
+            ProductRepository.GetByIdAsync(productId).ReturnsForAnyArgs(product);
+            
+            //Act            
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.UpdateProductAsync(
+                   productId,
+                   productName,
+                   inventory,
+                   price
+                );
+            });
+
+            //Assert
+            Assert.AreEqual(
+                $"El producto con ID {productId} fue eliminado anteriormente.",
+                ex.Message
+            );            
+            await ProductRepository.ReceivedWithAnyArgs(1).GetByIdAsync(Arg.Any<object>());
+        }
+
+        [TestMethod]
+        public async Task DeleteProductAsync_ProductNotExistFailed()
+        {
+            //Arrange            
+            int productId = 1;
+
+            //Act            
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.DeleteProductAsync(
+                   productId
+                );
+            });
+
+            //Assert
+            Assert.AreEqual(
+                $"El producto con ID {productId} no existe.",
+                ex.Message
+            );
+        }
+
+        [TestMethod]
+        public async Task DeleteProductAsync_ProductBeforeDeletedFailed()
+        {
+            //Arrange           
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .WithIsDeleted(true)
+                .Build();
+
+            ProductRepository.GetByIdAsync(productId).ReturnsForAnyArgs(product);
+
+            //Act            
+            AppException ex = await Assert.ThrowsExceptionAsync<AppException>(async () =>
+            {
+                await Service.DeleteProductAsync(
+                   productId
+                );
+            });
+
+            //Assert
+            Assert.AreEqual(
+                $"El producto con ID {productId} fue eliminado anteriormente.",
+                ex.Message
+            );
+            await ProductRepository.ReceivedWithAnyArgs(1).GetByIdAsync(Arg.Any<object>());
+        }
+        
+        [TestMethod]
+        public async Task DeleteProductAsync_Ok()
+        {
+            //Arrange           
+            int productId = 1;
+
+            Product product = ProductBuilder
+                .WithProductId(productId)
+                .WithIsDeleted(false)
+                .Build();
+
+            ProductRepository.GetByIdAsync(productId).ReturnsForAnyArgs(product);
+            ProductRepository.UpdateAsync(product).ReturnsForAnyArgs(product);
+            //Act            
+
+            await Service.DeleteProductAsync(
+                productId
+            );
+            
+            //Assert            
+            await ProductRepository.ReceivedWithAnyArgs(1).GetByIdAsync(Arg.Any<object>());
+            await ProductRepository.ReceivedWithAnyArgs(1).UpdateAsync(Arg.Any<Product>());
+        }        
+    }
+}
